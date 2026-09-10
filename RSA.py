865a510743b4734fb8d2d55927b2ee84edcb089c
Shravani <shravanibacchu@gmail.com>
2026-09-10 18:43:06 +0100

    feat: create a simple main function

--- RSA.py
+++ RSA.py
@@ -4,30 +4,17 @@ import random
 # initialises variables for inputs
 p = int(input("Enter an integer number"))
 q = int(input("Enter another integer number"))
-<<<<<<< HEAD
-e = int(input("Enter a value of e"))
-n = p *q
-phi = (p-1) * (q-1)
-tot = phi
-# function to find the greatest common divisor 
-=======
+
 n = p * q
 phi = (p - 1) * (q - 1)
 tot = phi
 
 
 # greatest common divisor -> think this is what's used to see if e is valid but idk
->>>>>>> refs/rewritten/main
 def gcd(p, q):
     while q != 0:
         p, q = q, p % q
     return p
-<<<<<<< HEAD
-# function to check whether e is a valid exponent
-def is_valid_e(e, tot):
-    return gcd(e, tot) == 1
-
-=======
 
 
 def is_valid_e(e, tot):
@@ -35,7 +22,6 @@ def is_valid_e(e, tot):
 
 
 e = int(input("Enter a value of e"))
->>>>>>> refs/rewritten/main
 if is_valid_e(e, tot) and (1 < e < tot):
     print("Yay, that works!")
 else:
@@ -72,8 +58,7 @@ def powerMod(a, b, n):
     return result
 
 
-<<<<<<< HEAD
-#primality test
+# primality test
 def primality_check(n, k=20):
     if n < 2:
         return False
@@ -88,11 +73,11 @@ def primality_check(n, k=20):
         d = d // 2
         r += 1
 
-    for _ in range(k):                    
+    for _ in range(k):
         a = random.randint(2, n - 2)
         x = powerMod(a, d, n)
         if x == 1 or x == n - 1:
-            continue                      
+            continue
 
         for _ in range(r - 1):
             x = powerMod(x, 2, n)
@@ -104,28 +89,23 @@ def primality_check(n, k=20):
     return True
 
 
-=======
-# primality-test function
-def primality_test(n):
-    if n < 2:
-        return False
-    elif n in (2, 3):
-        return True
-    elif n % 2 == 0:
-        return False
-    return None
+def generate_bits(bits, k=20):
+    while True:
+        candidate = random.randbits(bits)
+        candidate |= (1 << bits - 1) | 1
+        if primality_check(candidate, k):
+            return candidate
 
 
 def main():
-    mode = input("Enter 'E' or 'D' for the encrypt function or the decrypt function")
-    if mode =="E":
-        message = int(input("Enter the message to be encrypted"))
-        encrypted_msg = powerMod(message,e,n)
-        print(encrypted_msg)
-    elif mode == "D":
-        encrypted_msg = int(input("Enter the message to be decrypted"))
-        decrypted_function = powerMod(encrypted_msg,d,n)
-        print(decrypted_function)
-    else:
-        print("Invalid Function!, try again")
->>>>>>> refs/rewritten/main
+    bits = int(input("Enter bit-lengths for p and q"))
+    p = generate_bits(bits)
+    q = generate_bits(bits)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = int(input("Enter a value of e: "))
+    while not (is_valid_e(e, phi) and 1 < e < phi):
+        e = int(input("Enter a value of e: "))
+    print(f"The value of e chosen is {e}")
+    g, x, y = eea(e, phi)
+    d = x % phi
